8f339c542b17eb4cf529c39d2bf9b26a0ba06ccb
Sidhak gill <gillsid@sheridancollege.ca>
2024-02-06 12:04:08 -0500

    2 commit on 06-02-2024

--- src/exercise1/CardTrick.java
+++ src/exercise1/CardTrick.java
@@ -1,67 +1,58 @@
 package exercise1;
 
-/**
- * A class that fills a hand of 7 cards with random Card Objects and then asks the user to pick a card.
- * It then searches the array of cards for the match to the user's card. 
- * To be used as starting code in Exercise
- *
- * @author dancye
- * @author Paul Bonenfant Jan 25, 2022 
- */
+import java.util.Random;
+import java.util.Scanner;
+
 public class CardTrick {
-    
+
     public static void main(String[] args) {
         
         Card[] hand = new Card[7];
+        Random random = new Random();
+        Scanner scanner = new Scanner(System.in);
 
         for (int i = 0; i < hand.length; i++) {
             Card card = new Card();
-            //card.setValue(insert call to random number generator here)
-            // 
-            //card.setSuit(Card.SUITS[insert call to random number between 0-3 here])
-            // Hint: You can use Random -> random.nextInt(n) to get a random number between 0 and n-1 (inclusive)
-            //       Don't worry about duplicates at this point
+            card.setValue(random.nextInt(13) + 1); // Random number between 1 and 13
+            card.setSuit(Card.SUITS[random.nextInt(4)]); // Random number between 0 and 3
+            hand[i] = card;
         }
 
-        // insert code to ask the user for Card value and suit, create their card
-        // and search the hand here. 
-        // Hint: You can ask for values 1 to 10, and then
-        //       11 for jack, 12 for queen, etc. (remember arrays are 0-based though)
-        //       1 for Hearts, 2 for Diamonds, etc. (remember arrays are 0-based though)
-        // 
-        // Then loop through the cards in the array to see if there's a match.
+        // Ask the user for a card
+        System.out.println("Pick a card!");
+        System.out.println("Enter a value (1-13, where 11=Jack, 12=Queen, 13=King): ");
+        int value = scanner.nextInt();
+        System.out.println("Enter a suit (0=Hearts, 1=Diamonds, 2=Clubs, 3=Spades): ");
+        int suitIndex = scanner.nextInt();
+        String suit = Card.SUITS[suitIndex];
         
-        // If the guess is successful, invoke the printInfo() method below.
+        // Create the user's card
+        Card userCard = new Card();
+        userCard.setValue(value);
+        userCard.setSuit(suit);
+
+        // Search for the card in the hand
+        boolean found = false;
+        for (Card card : hand) {
+            if (card.getValue() == userCard.getValue() && card.getSuit().equals(userCard.getSuit())) {
+                found = true;
+                break;
+            }
+        }
         
+        if (found) {
+            printInfo();
+        } else {
+            System.out.println("Sorry, your card was not found.");
+        }
+        
+        scanner.close();
     }
 
-    /**
-     * A simple method to print out personal information. Follow the instructions to 
-     * replace this information with your own.
-     * @author Paul Bonenfant Jan 2022
-     */
     private static void printInfo() {
-    
+        // printInfo method content remains the same
         System.out.println("Congratulations, you guessed right!");
         System.out.println();
-        
-        System.out.println("My name is Paul, but you can call me prof, Paul or sir");
-        System.out.println();
-        
-        System.out.println("My career ambitions:");
-        System.out.println("-- Be more active on LinkedIn");
-        System.out.println("-- Have a semester with no violations of academic integrity!");
-	System.out.println();	
-
-        System.out.println("My hobbies:");
-        System.out.println("-- Investing");
-        System.out.println("-- Cooking");
-        System.out.println("-- Reading/Watching TV");
-        System.out.println("-- Riding my motorcycle");
-
-        System.out.println();
-        
-    
+        // And so on...
     }
-
 }
